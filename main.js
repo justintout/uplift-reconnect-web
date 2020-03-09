9230a1d5c43a97d7034d86747174381b83891e54
Justin Tout <3639630+justintout@users.noreply.github.com>
2020-03-09 09:45:12 -0400

    Add debug logging

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const deskDownPacket = new Uint8Array([0xf1, 0xf1, 0x02, 0x00, 0x02, 0x7e]);
 let desk;
 
 class Desk {
+    
     constructor(device) {
         this.device = device;
     }
@@ -50,6 +51,65 @@ async function disconnect() {
     desk = undefined;
 }
 
+// TODO: lots of conversions between dataview and array. maybe only 
+// convert once then act only on arrays after read...
+/**
+ * Converts a DataView of Uint8Array to an Array<number>
+ * @param {DataView} d 
+ * @returns {Array<number>}
+ */
+function toArray(d) {
+    let a = [];
+    for (let i = 0; i < d.byteLength; i++) {
+        a.push(d.getUint8(i))
+    }
+    return a;
+}
+
+/**
+ * Tests whether the given DataView of Uint8Array can be decoded to a "clean" ASCII string.
+ * "clean" in this context means it is entirely in the character range 32-126
+ * @param {DataView} d
+ * @returns {boolean}
+ */
+function isAsciiString(d) {
+    const a = toArray(d);
+    for (const b of a) {
+        if (b < 32 || b > 126) return false;
+    }
+    return true;
+}
+
+/**
+ * Converts a DataView of Uint8Array to a UTF-8 string
+ * @param {DataView} d 
+ * @returns {string}
+ */
+function toUTF8(d) {
+    const decoder = new TextDecoder('utf-8');
+    return decoder.decode(d);
+}
+
+/**
+ * Converts a DataView of Uint8Array to a hex string
+ * example: [1,2,3] => '010203'
+ * @param {DataView} d 
+ * @returns {string}
+ */
+function toHexString(d) {
+    return toArray(d).map(b => b.toString(16).padStart(2, '0')).join('');
+}
+
+/**
+ * Converts a DataView to a UTF-8 string surrounded in quotes if "clean", or a hex string
+ * @param {DataView} d 
+ * @returns {string}
+ */
+function toHexOrUTF8(d) {
+    if (isAsciiString(d)) return `"${toUTF8(d)}"`;
+    return toHexString(d);
+}
+
 (function() {
     document.querySelector('#btnServiceDiscovery').addEventListener('click', onDiscoverButtonClick);
     document.querySelector('#btnConnect').addEventListener('click', onConnectClick);
@@ -57,6 +117,7 @@ async function disconnect() {
     document.querySelector('#btnUp').addEventListener('mousedown', onUpMouseDown);
     document.querySelector('#btnDown').addEventListener('mousedown', onDownMouseDown);
 })();
+
 
 async function onDiscoverButtonClick() {
     alert('Check the console for info');
@@ -70,10 +131,33 @@ async function onDiscoverButtonClick() {
         const server = await device.gatt.connect();
         const services = await server.getPrimaryServices();
         for (const service of services) {
+            console.group(`Service: ${service.uuid} (${service.isPrimary ? 'is' : 'not'} primary)`);
             const characteristics = await service.getCharacteristics();
             for (const characteristic of characteristics) {
-                console.log(JSON.stringify(characteristic));
+                console.group(`Characteristic: ${characteristic.uuid}`);
+                if (characteristic.properties.read) {
+                    const v = await characteristic.readValue();
+                    console.log(`Value: ${toHexOrUTF8(v)}`);
+                }
+                console.log(`Read: ${characteristic.properties.read}`);
+                console.log(`Write: ${characteristic.properties.write}`);
+                console.log(`Write w/o response: ${characteristic.properties.writeWithoutResponse}`);
+                console.log(`Notify: ${characteristic.properties.notify}`);
+                console.log(`Indicate: ${characteristic.properties.indicate}`);
+                console.log(`Broadcast: ${characteristic.properties.broadcast}`);
+                console.log(`Signed write: ${characteristic.properties.authenticatedSignedWrites}`);
+                console.log(`Queued write: ${characteristic.properties.reliableWrite}`);
+                console.log(`Writable auxiliaries: ${characteristic.properties.writableAuxiliaries}`);
+                const descriptors = await characteristic.getDescriptors();
+                console.group(`Descriptors: `);
+                for (const descriptor of descriptors) {
+                    const value = await descriptor.readValue();
+                    console.log(`${descriptor.uuid} = ${toHexOrUTF8(value)}`);
+                }
+                console.groupEnd();
+                console.groupEnd();
             }
+            console.groupEnd();
         }
     } catch (e) {
         console.error(`couldn't do device discovery: ${e}`);
